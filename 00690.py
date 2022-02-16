5bb9bdde1824e8456f22aa13c6e0d07de00a67c8
Jiming Dai <dai.michael@gmail.com>
2022-02-16 09:54:03 -0600

    Solution for problem 690

---
+++ 00690.py
@@ -0,0 +1,21 @@
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: list[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+
+
+class Solution:
+    def getImportance(self, employees: list['Employee'], id: int) -> int:
+        pass
+
+
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
