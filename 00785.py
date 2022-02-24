290ff3366dcfae9d589f378825779a9936949b5c
Jiming Dai <dai.michael@gmail.com>
2022-02-24 12:07:49 -0600

    Update solution to problem 785

--- 00785.py
+++ 00785.py
@@ -1,20 +1,21 @@
 class Solution:
-    def isBipartite(self, graph: list[list[int]]) -> bool:
+    def isBipartite2(self, graph: list[list[int]]) -> bool:
         RED  = 1
         BLUE = 2
 
         g = {}
-        for ii in range(len(graph)):
-            for jj in graph[ii]:
+        for ii, jj in enumerate(graph):
+            for kk in jj:
                 if ii in g:
-                    g[ii].append(jj)
+                    g[ii].append(kk)
                 else:
-                    g[ii] = [jj]
+                    g[ii] = [kk]
 
         is_possible = True
         curr_color = RED
         next_color = BLUE
         color = [0] * len(graph)
+
         def dfs(node):
             nonlocal is_possible
             nonlocal curr_color
@@ -36,12 +37,31 @@ class Solution:
             if color[ii] == 0:
                 dfs(ii)
 
-
         return is_possible
 
 
+    # The idea is that each node has to be in a different set than its neighbors, and all its neighbors should be in the same set.
+    def isBipartite(self, graph: list[list[int]]) -> bool:
+        def find(x: int) -> int:
+            while root[x] != x:
+                x = root[x]
+            return x
 
+        def union(x: int, y: int):
+            rootx = find(x)
+            rooty = find(y)
+            root[rootx] = rooty
 
+        root = [ii for ii in range(len(graph))]
+
+        for node, neighbors in enumerate(graph):
+            for ii in neighbors:
+                if find(ii) == find(node):
+                    return False
+            for jj, kk in zip(neighbors, neighbors[1:]):
+                union(jj, kk)
+
+        return True
 
 
 def main():
@@ -54,6 +74,9 @@ def main():
 
     graph = [[4],[],[4],[4],[0,2,3]]
     print("3: " + str(s.isBipartite(graph)))
+
+    graph = [[3], [2, 4], [1], [0, 4], [1, 3]]
+    print("4: " + str(s.isBipartite(graph)))
 
     return
 
