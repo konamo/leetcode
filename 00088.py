b264a771b1af8ba8da9e197e0c0f64b858365053
Jiming Dai <dai.michael@gmail.com>
2022-04-03 13:15:20 -0500

    Update solution to problem 88

--- 00088.py
+++ 00088.py
@@ -1,5 +1,5 @@
 class Solution:
-    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    def merge3(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -14,13 +14,24 @@ class Solution:
                 n -= 1
         if n > 0:
             nums1[:n] = nums2[:n]
-
+        return
 
     def merge2(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         nums1[m:] = nums2
         return nums1.sort()
 
-
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while n > 0:
+            if m > 0 and nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        return
 
 
 def main():
