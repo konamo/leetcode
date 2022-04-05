35a8f32ba5ff91e19bdcb41c45ccdbda87b531a8
Jiming Dai <dai.michael@gmail.com>
2022-04-05 17:27:32 -0500

    Add solution to problem 540

---
+++ 00540.py
@@ -0,0 +1,32 @@
+class Solution:
+    def singleNonDuplicate2(self, nums: list[int]) -> int:
+        s = [ii for ii in nums if nums.count(ii) == 1]
+        return s[0]
+
+    def singleNonDuplicate(self, nums: list[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1 and nums[mid - 1] == nums[mid]:
+                left = mid + 1
+            elif mid % 2 == 0 and nums[mid + 1] == nums[mid]:
+                left = mid + 2
+            else:
+                right = mid
+        assert(left == right)
+        return nums[left]
+
+
+def main():
+    s = Solution()
+    nums = [1,1,2,3,3,4,4,8,8]
+    print(s.singleNonDuplicate(nums))
+
+    nums = [3,3,7,7,10,11,11]
+    print(s.singleNonDuplicate(nums))
+    return
+
+
+
+if __name__ == '__main__':
+    main()
