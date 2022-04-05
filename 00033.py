609e792a45c878d1ef4e3a3bee8bc3dc1db69128
Jiming Dai <dai.michael@gmail.com>
2022-04-05 14:17:01 -0500

    Update solution to problem 33

--- 00033.py
+++ 00033.py
@@ -1,5 +1,5 @@
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
+    def search2(self, nums: list[int], target: int) -> int:
         L, H = 0, len(nums)
         while L < H:
             M = (L+H) // 2
@@ -15,6 +15,29 @@ class Solution:
                 return M
         return -1
 
+    def search(self, A: List[int], target: int) -> int:
+        n = len(A)
+        left, right = 0, n - 1
+        if n == 0: return -1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if A[mid] == target: return mid
+            
+            # inflection point to the right. Left is strictly increasing
+            if A[mid] >= A[left]:
+                if A[left] <= target < A[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                    
+            # inflection point to the left of me. Right is strictly increasing
+            else:
+                if A[mid] < target <= A[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
 
 def main():
     s = Solution()
