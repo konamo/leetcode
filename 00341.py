71a812d4193b7c10d542d05e53b9940972df8e68
Jiming Dai <dai.michael@gmail.com>
2022-04-08 11:36:39 -0500

    Add solution to problem 341

---
+++ 00341.py
@@ -0,0 +1,69 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.index = -1
+        self.list = []
+
+
+        def dfs(l):
+            for ii in l:
+                if ii.isInteger():
+                    self.list.append(ii.getInteger())
+                else:
+                    dfs(ii.getList())
+
+        dfs(nestedList)
+        return
+        
+    
+    def next(self) -> int:
+        self.index += 1
+        return self.list[self.index]
+        
+    
+    def hasNext(self) -> bool:
+        return len(self.list[self.index+1:]) > 0
+        
+
+
+
+    def __init__(self, nestedList: [NestedInteger]):
+        self.list = nestedList
+        return
+        
+    
+    def next(self) -> int:
+        return self.list.pop(0).getInteger()
+        
+    
+    def hasNext(self) -> bool:
+        while self.list:
+            first = self.list[0]
+            if first.isInteger():
+                return True
+            self.list = first.getList() + self.list[1:]
+        return False
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
