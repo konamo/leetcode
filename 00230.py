04eb353f41c7d9d49546647cd9dcb34bd6e4ea19
Jiming Dai <dai.michael@gmail.com>
2022-04-08 09:41:29 -0500

    Update solution to problem 230

--- 00230.py
+++ 00230.py
@@ -21,3 +21,22 @@ class Solution:
 
         dfs(root)
         return ret[k-1]
+
+
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
