4f6b820955db9660d0a6f31d03d9140bc543c017
Jiming Dai <dai.michael@gmail.com>
2022-04-28 19:19:25 -0500

    Add c solution to problem 141

---
+++ 00141.c
@@ -0,0 +1,17 @@
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *slow, *fast;
+
+    slow = head;
+    fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
