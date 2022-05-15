752378189bd00e6f5105ab1632e0817b15bd7a38
Jiming Dai <dai.michael@gmail.com>
2022-05-15 18:08:51 -0500

    Update solution for problem 240

--- 00240.c
+++ 00240.c
@@ -19,3 +19,28 @@ bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target)
     }
     return false;
 }
+
+
+
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target)
+{
+    if (matrix == NULL) {
+        return false;
+    }
+
+    // start from left bottom corner
+    int col = 0;
+    int row = matrixSize - 1;
+
+    while (col < *matrixColSize && row >= 0) {
+        if(target == matrix[row][col]) {
+            return true;
+        } else if(target < matrix[row][col]) {
+            row--;
+        } else if(target > matrix[row][col]) {
+            col++;
+        }
+    }
+    return false;
+}
